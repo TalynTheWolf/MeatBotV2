c6ce76bba950190e9e48cff7702e06dff72ee440
Talyn <139556386+TalynTheWolf@users.noreply.github.com>
2023-11-20 05:15:38 +0000

    Discord.js is fucking retarded

--- commands/Cry.js
+++ commands/Cry.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('Cry')
+        .setName('cry')
         .setDescription('Sends "Cry About It Platypus" into the chat, because its funny'),
     async execute(interaction) {
         await interaction.reply("https://tenor.com/en-GB/view/cry-about-it-cry-about-it-beaver-gif-20935280");
--- commands/alucard.js
+++ commands/alucard.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('Alucard')
+        .setName('alucard')
         .setDescription('Shows a ship of all time!!!111!!!1'),
     async execute(interaction) {
         await interaction.reply("https://media.discordapp.net/attachments/838385780421427260/1155679430845419571/image.png");
