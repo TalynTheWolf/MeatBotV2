5efdcaba2c06611ced4c6f24485c076e1e72331d
Talyn <139556386+TalynTheWolf@users.noreply.github.com>
2024-04-03 22:16:05 +0100

    Added a command to send a message as the bot.

---
+++ commands/meattalk.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('meattalk')
+        .setDescription('Allows staff to send a message as MeatBot, for some reason idk')
+        .addStringOption(option => 
+            option
+                .setName('text')
+                .setDescription('Text to send as MeatBot')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+
+        async execute(interaction) {
+            const MeatText = interaction.options.getString('text')
+            
+            await interaction.reply({ content: 'Sending as MeatBot...', ephemeral: true });
+            await interaction.followUp(MeatText);
+        }
+}
